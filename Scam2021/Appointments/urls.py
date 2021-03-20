3fe3c26c2243dbb4b88ce0f1e8b225c322b30f03
aakash2410 <aakash.sangani2410@gmail.com>
2021-03-20 18:04:45 +0530

    Initial (again)

--- Scam2021/Appointments/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('book/<int:doctor_id>/', book),
-    path('approve/', approve),
-    path('find_doctor/', find_doctor)
-]
