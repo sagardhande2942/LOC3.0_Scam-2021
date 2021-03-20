10a87afb14c370b5113d4699dad200ae19436174
aakash2410 <aakash.sangani2410@gmail.com>
2021-03-20 17:04:23 +0530

    Models fixed

--- Scam2021/Auth/models.py
+++ Scam2021/Auth/models.py
@@ -2,13 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User 
 
 
-class Users(models.Model):
-    email = models.EmailField(default='')
-    id = models.AutoField(primary_key=True)
-    firstname = models.CharField(max_length=20,default='',null = True, blank=True)
-    lastname = models.CharField(max_length=20, default='',null = True, blank=True)
-    password = models.CharField(max_length=30, default='')
+class Users(User):
     contact = models.IntegerField(default='',null = True, blank=True)
+    
     def __str__(self):
         return self.first_name
 
